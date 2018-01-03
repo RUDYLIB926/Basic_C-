02ffe7b9d426fac7fef59a953040caab44936592
RUDYLIB926 <rlibertini3@gmail.com>
2018-01-02 21:22:22 -0500

    Typing Game

---
+++ HeadFirstChapter4/TypeFasterGame/TypeFasterGame/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypeFasterGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Add a randm key to the ListBox
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if(listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game over! Press space to play again.");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If the user pressed a key that's in the ListBox, remove it
+            // and then make the game a little faster
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+                difficultyProgressBar.Value = 800 - timer1.Interval;
+
+                // The user pressed a correct key
+                stats.Update(true);
+            }
+            else
+            {
+                // The user pressed an incorrect key
+                stats.Update(false);
+            }
+
+            if(e.KeyValue == 32)
+            {
+                SpaceBarPressed();
+            }
+
+            // Update Form1 labels
+            correctLabel.Text = "Correct: " + stats.Correct;
+            missedLabel.Text = "Missed: " + stats.Missed;
+            totalLabel.Text = "Total: " + stats.Total;
+            accuracyLabel.Text = "Accuracy: " + stats.Accuracy + "%";
+        }
+
+        private void SpaceBarPressed()
+        {
+            stats.Clear();
+            listBox1.Items.Clear();
+            timer1.Interval = 800;
+            timer1.Start();
+        }
+    }
+}
